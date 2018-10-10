311e530b703bf200b52309a3b19a7ef482363025
bodanky.designs@gmail.com <bodanky.designs@gmail.com>
2018-10-09 19:11:38 -0500

    start of week 6 exercise

---
+++ js/script.js
@@ -0,0 +1,51 @@
+console.log('sup');
+//place.action("stuff")
+
+// if under 21 but over 18 can enter but cannot drink, if if under 18 cant enter at all, if over 21 its whatever?
+var userAge = prompt("How old are you?");
+// this was my code before watching the video 
+// if(userAge > 21) {
+//     console.log("You can enter!, Move along with it!");
+// } else if(userAge < 21 && userAge > 18) {
+//     console.log("You can enter but you cannot drink good sir/madam!");
+// } else {
+//     console.log("You can't enter at all!");
+// }
+
+// if(userAge > 21) {
+//     console.log("You can enter!, Move along with it!");
+// } else if(userAge == 21) {
+//     console.log("congrats on turning 21! have a great time!")
+// } else if(userAge > 18) {
+//     console.log("You can enter but you cannot drink good sir/madam!");
+// } else if(userAge == 18) {
+//     console.log("congrats on turning 18! have a great time! But you can't drink, sorry pal");
+// } else {
+//     console.log("You can't enter at all!");
+// }
+
+
+
+//Functions 
+
+
+// A function that doesn't take a argument
+function greet() {
+    alert(greeting);
+}
+
+// A function that takes a argument 
+function sum(x, y) {
+    console.log(x + y);
+}
+
+
+
+
+
+
+
+
+
+
+
